ce2c71994de24371a8dc0784a318252a5dd9a16f
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-10-26 17:17:11 +0300

    Create custom_deleter_01.cpp

---
+++ shared_ptr/custom_deleter_01.cpp
@@ -0,0 +1,16 @@
+#include <memory>
+#include "person.h"
+
+
+int main()
+{
+	{
+		std::shared_ptr<Person> sptr{ new Person{"Gul", "Eryaman"}, [](Person* p) {
+									std::cout << *p << " kisisi delete ediliyor\n";
+									delete p;
+										} };
+	}
+
+	std::cout << "main devam ediyor\n";
+
+}
