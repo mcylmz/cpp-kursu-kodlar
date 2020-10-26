dda0eae4a8b3f43ee2d771b8e64a93f846de613a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-10-26 17:24:22 +0300

    Create in_container_01.cpp

---
+++ shared_ptr/in_container_01.cpp
@@ -0,0 +1,31 @@
+#include <memory>
+#include "person.h"
+#include <list>
+#include <vector>
+
+int main()
+{
+	{
+		std::list<std::shared_ptr<Person>> mylist;
+
+		mylist.emplace_back(new Person{ "Ahmet", "Yesil" });
+		mylist.emplace_back(new Person{ "Ayse", "Topal" });
+		mylist.emplace_back(new Person{ "Veli", "Dundar" });
+		mylist.emplace_back(new Person{ "Murat", "Salih" });
+		std::cout << "bir tusa basin: "; getchar();
+
+		{
+			std::vector<std::shared_ptr<Person>> myvec(mylist.begin(), mylist.end());
+		////
+			for (auto& s : myvec)
+				s->set_name(s->Name() + "can");
+		}
+	
+		for (auto& s : mylist)
+			std::cout << *s << "\n";
+	
+		std::cout << "bir tusa basin: "; getchar();
+	}
+
+	std::cout << "main devam ediyor\n";
+}
