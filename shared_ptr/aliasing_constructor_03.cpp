3c4abeeb574c2150592a6a168e334736d05f0096
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-10-26 15:16:02 +0300

    Update aliasing_constructor_03.cpp

--- shared_ptr/aliasing_constructor_03.cpp
+++ shared_ptr/aliasing_constructor_03.cpp
@@ -1,6 +1,5 @@
 #include <memory>
 #include <vector>
-#include <conio.h>
 #include <iostream>
 
 
@@ -15,22 +14,12 @@ struct Owner
 	Member mx;
 };
 
-void func()
-{
-	shared_ptr<Owner> spo(new Owner); // use_count == 1
-	shared_ptr<Member> spm(spo, &spo->mx); // use_count == 2
-
-	spo.reset(); // use_count == 1
-
-	// When d goes out of scope, object is deleted.
-}
 
 using Ovec = std::vector<Owner>;
 
 
 int main()
 {
-
 	shared_ptr<Ovec> os(new Ovec); // use_count == 1
 	os->push_back(Owner());
 	os->push_back(Owner());
@@ -39,12 +28,9 @@ int main()
 	shared_ptr<Owner> o2(os, &os->at(1)); // use_count == 3
 	shared_ptr<Member> o3(os, &os->at(1).mx); // use_count == 3
 	std::cout << os.use_count() << "\n";
-	_getch();
 
 	os.reset(); // use_count == 2
 
 	// When o1 goes out of scope, use_count becomes 1.
 	// When o2 goes out of scope, objects is deleted.
 }
-//--------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------
