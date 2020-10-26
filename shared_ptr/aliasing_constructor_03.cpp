e8338d256453caddb09e5b2690b666342376cc71
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-10-26 15:26:39 +0300

    Update aliasing_constructor_03.cpp

--- shared_ptr/aliasing_constructor_03.cpp
+++ shared_ptr/aliasing_constructor_03.cpp
@@ -11,6 +11,10 @@ struct Member {
 
 struct Owner
 {
+	~Owner()
+	{
+		std::cout << "owner destructor\n";
+	}
 	Member mx;
 };
 
@@ -24,13 +28,31 @@ int main()
 	os->push_back(Owner());
 	os->push_back(Owner());
 
-	shared_ptr<Owner> o1(os, &os->at(0)); // use_count == 2
-	shared_ptr<Owner> o2(os, &os->at(1)); // use_count == 3
+	cout << "[1] os.use_count() = " << os.use_count() << "\n";
+
+	shared_ptr<Owner> o1(os, &os->at(0));
+	cout << "[2] os.use_count() = " << os.use_count() << "\n";
+	cout << "[3] o1.use_count() = " << o1.use_count() << "\n";
+	shared_ptr<Owner> o2(os, &os->at(1));
+	cout << "[4] os.use_count() = " << os.use_count() << "\n";
+	cout << "[5] o1.use_count() = " << o1.use_count() << "\n";
+	cout << "[6] o2.use_count() = " << o2.use_count() << "\n";
 	shared_ptr<Member> o3(os, &os->at(1).mx); // use_count == 3
-	std::cout << os.use_count() << "\n";
 
-	os.reset(); // use_count == 2
+	cout << "[7] os.use_count() = " << os.use_count() << "\n";
+	cout << "[8] o1.use_count() = " << o1.use_count() << "\n";
+	cout << "[9] o2.use_count() = " << o2.use_count() << "\n";
+	cout << "[10] o3.use_count() = " << o3.use_count() << "\n";
 
-	// When o1 goes out of scope, use_count becomes 1.
-	// When o2 goes out of scope, objects is deleted.
+	os.reset();
+
+	cout << "[11] os.use_count() = " << os.use_count() << "\n";
+	cout << "[12] o1.use_count() = " << o1.use_count() << "\n";
+	cout << "[13] o2.use_count() = " << o2.use_count() << "\n";
+	cout << "[14] o3.use_count() = " << o3.use_count() << "\n";
+	cout << "bir tusa basin ";
+	getchar();
+
 }
+//--------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------
