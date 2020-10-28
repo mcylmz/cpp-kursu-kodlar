744b94b726f0a3400e4307b1fddbb701d2b89c9d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-10-28 17:58:55 +0300

    Create manipulators_01.cpp

---
+++ iostream/formatting/manipulators_01.cpp
@@ -0,0 +1,19 @@
+#include <ostream>
+
+std::ostream& Boolalpha(std::ostream& os)
+{
+	os.setf(std::ios::boolalpha);
+	return os;
+}
+
+std::ostream& NoBoolalpha(std::ostream& os)
+{
+	os.unsetf(std::ios::boolalpha);
+	return os;
+}
+
+std::ostream& Hex(std::ostream& os)
+{
+	os.setf(std::ios::hex, std::ios::basefield);
+	return os;
+}
