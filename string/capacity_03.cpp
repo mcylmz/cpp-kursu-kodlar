9b2133c13020201c2c2d66064f7bbb3ea6fb210a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-11-01 22:22:57 +0300

    Create capacity_03.cpp

---
+++ string/capacity_03.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	string s;
+	s.reserve(700'000);
+	auto cap = s.capacity();
+	int alloc_cnt{};
+	
+	for (int i = 0; i < 1'000'000; ++i) {
+		s.push_back('a');
+		auto new_cap = s.capacity();
+		if (new_cap > cap) {
+			std::cout << ++alloc_cnt << ". reallocation size : " << s.size() << "  capacity : " << s.capacity() << "\n";
+			cap = new_cap;
+		}
+	}
+}
+
