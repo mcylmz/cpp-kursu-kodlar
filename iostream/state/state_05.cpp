929ae250537d37e1171133af29263d8afc14120f
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-11-02 15:43:35 +0300

    Update state_05.cpp

--- iostream/state/state_05.cpp
+++ iostream/state/state_05.cpp
@@ -2,18 +2,23 @@
 #include <iomanip>
 #include <string>
 
-
 int main()
 {
 	using namespace std;
-	
+
 	int x{};
 
 	while (std::cout << "bir tamsayi girin: " && !(std::cin >> x)) {
-		cin.clear();
-		string line;
-		getline(cin, line);
-		cout << "[" << line << "] gecerli tam sayi degil\n";
+		if (cin.eof()) {
+			std::cout << "giris yapmadiniz...\n";
+			cin.clear();
+		}
+		else {
+			cin.clear();
+			string line;
+			getline(cin, line);
+			cout << "[" << line << "] gecerli tam sayi degil\n";
+		}
 	}
 
 	cout << "giris basarili : " << x << "\n";
