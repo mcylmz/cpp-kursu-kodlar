383441d60399e2ebc784370663a1ce8a9d835b71
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-11-02 17:37:31 +0300

    Create seek_03.cpp

---
+++ iostream/seek/seek_03.cpp
@@ -0,0 +1,22 @@
+#include <sstream>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	using namespace std;
+
+	ostringstream oss{ "*necati ergin" };
+
+	cout << "[" << oss.str() << "]\n";
+
+	oss.seekp(7);
+	oss.put('-');
+	cout << "[" << oss.str() << "]\n";
+	oss.seekp(0, ios::end);
+	oss.put('!');
+	cout << "[" << oss.str() << "]\n";
+	oss.seekp(0);
+	oss.put('!');
+	cout << "[" << oss.str() << "]\n";
+
+}
