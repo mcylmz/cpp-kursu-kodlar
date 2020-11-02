f0b8d5d3f4c04c00b5f8a6e0d6fedcc6b6f8810f
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-11-02 15:35:21 +0300

    Create istringstream_04.cpp

---
+++ iostream/stringstream/istringstream_04.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+
+int main()
+{
+	using namespace std;
+	string str;
+	cout << "sayilar giriniz: ";
+	getline(cin, str);
+
+	istringstream iss{ str };
+
+	int ival;
+	int sum{};
+
+	while (iss >> ival) {
+		sum += ival;
+	}
+
+	std::cout << "toplam = " << sum << "\n";
+}
