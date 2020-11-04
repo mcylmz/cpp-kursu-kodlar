229e198d47c3813504cb1d300518c258f2af5c98
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-11-04 18:58:52 +0300

    Create mp_01.cpp

---
+++ member_pointers/mp_01.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+class Myclass {
+
+public:
+	Myclass(int a) : mx{ a } {}
+	void set(int x)
+	{
+		std::cout << "Myclass::set\n";
+		mx = x;
+	}
+
+	void print()const
+	{
+		std::cout << "mx = " << mx << "\n";
+	}
+private:
+	int mx;
+};
+
+int main()
+{
+	//void (Myclass::*fptr)(int) = &Myclass::set;
+	auto fptr = &Myclass::set;
+	Myclass x{ 20 };
+	x.print();
+	(x.*fptr)(50);
+
+	x.print();
+}
+
