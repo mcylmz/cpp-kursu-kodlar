a2a2a335bdc8ea6eb204f4699022c14c9fdae48f
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-11-09 18:14:09 +0300

    Create distribution_02.cpp

---
+++ random/distribution_02.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <random>
+
+std::mt19937 &eng()
+{
+	static std::mt19937 eng{ std::random_device{}() };
+
+	return eng;
+}
+
+int main()
+{
+	std::uniform_int_distribution<int> dist;
+
+	for (int i = 0; i < 10; ++i)
+		std::cout << dist(eng()) << '\n';
+
+	return 0;
+}
