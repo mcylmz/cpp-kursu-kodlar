23e31db0563824611a37dac602394364018c0c2c
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-11-09 13:10:08 +0300

    Create duration_10.cpp

---
+++ chrono/duration_10.cpp
@@ -0,0 +1,29 @@
+#include <chrono>
+#include <iostream>
+
+template<typename R, typename P>
+std::ostream& operator<<(std::ostream& os, const std::chrono::duration<R, P>& dur)
+{
+	return os << dur.count() << " * (" << P::num << " / " << P::den << ")";
+}
+
+using namespace std;
+using namespace chrono;
+
+using halfsec = std::chrono::duration<long long, std::ratio<1, 2>>;
+using frame = std::chrono::duration<double, std::ratio<1, 10>>;
+
+using Day = std::chrono::duration<int, std::ratio<60 * 60 * 24>>;
+
+int main()
+{
+	halfsec x{ 128 };
+
+	++x;
+	++x;
+	++x;
+
+	cout << x.count() << "\n";
+	cout << milliseconds{ x }.count() << "\n";
+	cout << x << "\n" << milliseconds{ x } << "\n";
+}
