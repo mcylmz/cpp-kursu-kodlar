4c76491614604a1c5ce0463f0e310115f6f822be
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-11-19 15:56:31 +0300

    Create noexcept_operator_02.cpp

---
+++ exception/noexcept_operator_02.cpp
@@ -0,0 +1,25 @@
+//Bu örnek noexcept operatörü ile ilgilidir.
+//noexcept operatörü noexcept belirteci (specifier) ile karıştırılmamalı
+
+#include <iostream>
+
+void f1();
+void f2()noexcept;
+auto fx = [] {};
+auto fy = []()noexcept {};
+
+
+class Nec {
+public:
+	~Nec() {};
+};
+int main()
+{
+	std::cout << std::boolalpha;
+	std::cout << noexcept(f1()) << "\n";
+	std::cout << noexcept(f2()) << "\n";
+	std::cout << noexcept(fx()) << "\n";
+	std::cout << noexcept(fy()) << "\n";
+	std::cout << noexcept(std::declval<Nec>().~Nec()) << "\n";
+	constexpr auto flag = noexcept(fx()); //gecerli
+}
