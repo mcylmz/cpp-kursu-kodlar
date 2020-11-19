22faa9456f54745ea83270b25aece8e9a2661470
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-11-19 15:51:09 +0300

    Create rethrow_02.cpp

---
+++ exception/rethrow_02.cpp
@@ -0,0 +1,29 @@
+#include <exception>
+#include <stdexcept>
+#include <iostream>
+
+void func()
+{
+	try {
+		throw std::out_of_range{ "range hatasi" };
+	}
+	catch (const std::exception& ex) {
+		std::cout << "hata func icinde yakalandi : " << ex.what() << "\n";
+		throw ex;
+	}
+}
+
+
+int main()
+{
+	try {
+		func();
+	}
+	catch (const std::out_of_range&) {
+		std::cout << "hata yakalandi  (std::out_of_range)\n";
+	}
+
+	catch (const std::exception&) {
+		std::cout << "hata yakalandi (std::exception)\n";
+	}
+}
