244e7807994c3a34f10bc6aed09311bc6a0927af
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-12-03 11:55:14 +0300

    Create visit_01.cpp

---
+++ variant/visit_01.cpp
@@ -0,0 +1,11 @@
+#include <variant>
+#include <iostream>
+
+int main()
+{
+    std::variant<char, int, double> var = 'a';
+    const auto f{ [](auto x) { std::cout << x << '\n'; } };
+    std::visit(f, var);
+    var = 123;
+    std::visit(f, var);
+}
