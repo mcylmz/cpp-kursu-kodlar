7e2e58f22aa0ba51ddf662c83298e3e0c9ca7c9d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-12-04 15:33:01 +0300

    Update init_capture_005.cpp

--- lambda/init_capture_005.cpp
+++ lambda/init_capture_005.cpp
@@ -5,9 +5,15 @@
 int main()
 {
 	using namespace std;
-		
+
 	auto up = make_unique<string>("necati");
 	auto f = [up = move(up)]{ return *up + " ergin"; };
 
 	cout << f() << "\n";
+
+	if (up)
+		std::cout << "up dolu\n";
+	else
+		std::cout << "up bos\n";
+
 }
