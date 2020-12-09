41861d50123a46e0a0d3d49cdecb71572861f4c7
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-12-09 22:15:16 +0300

    Create ctad_01.cpp

---
+++ ctad/ctad_01.cpp
@@ -0,0 +1,15 @@
+#include <utility>
+
+template<typename T>
+class Myclass {
+public:
+	Myclass() = default;
+	Myclass(T);
+};
+
+int main()
+{
+	std::pair px{ 12, 5.6 };  
+	Myclass m{ 10 };  //m is Myclass<int>
+
+}
