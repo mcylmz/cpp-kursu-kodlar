4a7d5be9512bcc3c72bccfabb8677b80fbd7e6d7
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-12-11 18:26:03 +0300

    Update sw1.cpp

--- string_view/sw1.cpp
+++ string_view/sw1.cpp
@@ -1,1 +1,16 @@
-daha sonra
+#include <string_view>
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	std::cout << "sizeof (char *) = " << sizeof(char *) << "\n";
+	std::cout << "sizeof (size_t) = " << sizeof(size_t) << "\n";
+	std::cout << "sizeof (string_view) = " << sizeof(string_view) << "\n";
+}
+
+/*
+	bir string_view nesnesi aslında 2 pointer değişken (ya da bir pointer değişken bir de tamsayı) tutar.
+	Böylece mülkiyeti bir başka kodda olan bir yazının bir parçasına (substring) okuma amaçlı erişebilir.
+*/
