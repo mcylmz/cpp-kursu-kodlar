fdcce06c36520e7c58fcd0409062b4918accb7de
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-01-02 10:55:14 +0300

    Create reference__wrapper_03.cpp

---
+++ functional/reference__wrapper_03.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+
+struct Biggie 
+{
+	bool operator()(int i) const 
+	{
+		//code
+		return true;
+	}
+	//very big data
+};
+
+const Biggie gbig; 
+
+std::vector<int> ivec; 
+
+int main()
+{
+	using namespace std;
+
+	vector<int> destvec;
+	std::copy_if(ivec.begin(), ivec.end(), back_inserter(destvec), ref(gbig));
+}
