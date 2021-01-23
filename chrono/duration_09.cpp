37ca20182ac8d5a84cc0df52d70da8d43b9713c4
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-01-23 07:22:21 +0300

    Update duration_09.cpp

--- chrono/duration_09.cpp
+++ chrono/duration_09.cpp
@@ -11,9 +11,10 @@ std::ostream& operator<<(std::ostream& os, const std::chrono::duration<R, P>& du
 using namespace std;
 using namespace chrono;
 
-using halfsec	        = std::chrono::duration<long long, std::ratio<1, 2>>;
-using frame		= std::chrono::duration<double, std::ratio<1, 10>>;
-using Day		= std::chrono::duration<int, std::ratio<60 * 60 * 24>>;
+using halfsec = std::chrono::duration<long long, std::ratio<1, 2>>;
+using tensec = std::chrono::duration<long long, std::deca>;
+using frame = std::chrono::duration<double, std::ratio<1, 10>>;
+using Day = std::chrono::duration<int, std::ratio<60 * 60 * 24>>;
 
 int main()
 {
@@ -23,7 +24,7 @@ int main()
 	cout << 45ns << "\n";
 
 	cout << halfsec{ 128 } << "\n";
+	cout << tensec{ 5} << "\n";
 	cout << frame{ 3.61 } << "\n";
 	cout << Day{ 5 } << "\n";
 }
-
