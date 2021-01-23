dea847e346832d37fcddcd61a33b91604efb7662
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-01-23 07:50:02 +0300

    Update duration_05.cpp

--- chrono/duration_05.cpp
+++ chrono/duration_05.cpp
@@ -8,8 +8,9 @@ int main()
 {
 	using dsec = duration<double>;
 
+	cout << boolalpha;
 	cout << (543ms == 543000us) << "\n";
 	cout << (543ms < 543001us) << "\n";
-	cout << (543ms < 543001us) << "\n";
+	cout << (1s > dsec{ 0.999 }) << "\n";
 	cout << (dsec{ 0.5 } < 543ms) << "\n";
 }
