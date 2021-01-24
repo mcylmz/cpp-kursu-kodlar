9d780b9fd4c648a0d572a0de7b6c8ba13b47ff24
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-01-24 06:54:31 +0300

    Update print_engine_info_01.cpp

--- random/print_engine_info_01.cpp
+++ random/print_engine_info_01.cpp
@@ -1,4 +1,3 @@
-
 //to be compiled by MS compiler
 
 #include <ostream>
@@ -7,7 +6,7 @@
 #include <random>
 
 template<typename RG>
-void print_engine_info(std::ostream& os, const std::string name, RG&&)
+void print_engine_info(std::ostream& os, const std::string name)
 {
 	os << std::left << std::setw(16) << name << " = " << typeid(RG).name() << '\n';
 }
@@ -18,12 +17,12 @@ int main()
 {
 	using namespace std;
 
-	print_engine_info(cout, "minstd_rand0", minstd_rand0{});
-	print_engine_info(cout, "minstd_rand", minstd_rand{});
-	print_engine_info(cout, "mt19937", mt19937{});
-	print_engine_info(cout, "mt19937-64", mt19937_64{});
-	print_engine_info(cout, "ranlux24", ranlux24{});
-	print_engine_info(cout, "ranlux24_base", ranlux24_base{});
-	print_engine_info(cout, "ranlux48", ranlux48{});
-	print_engine_info(cout, "ranlux48_base", ranlux48_base{});
+	print_engine_info<minstd_rand0>(cout, "minstd_rand0");
+	print_engine_info<minstd_rand>(cout, "minstd_rand");
+	print_engine_info<mt19937>(cout, "mt19937");
+	print_engine_info<mt19937_64>(cout, "mt19937-64");
+	print_engine_info<ranlux24>(cout, "ranlux24");
+	print_engine_info<ranlux24_base>(cout, "ranlux24_base");
+	print_engine_info<ranlux48>(cout, "ranlux48");
+	print_engine_info<ranlux48_base>(cout, "ranlux48_base");
 }
