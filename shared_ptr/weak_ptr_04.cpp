07327584604d7797732de5bee98994a3cacae31e
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-02-04 14:26:19 +0300

    Update weak_ptr_04.cpp

--- shared_ptr/weak_ptr_04.cpp
+++ shared_ptr/weak_ptr_04.cpp
@@ -2,20 +2,18 @@
 #include "person.h"
 #include <iostream>
 
-
 using namespace std;
-
 
 int main()
 {
 	auto spx = make_shared<Person>("Hakki", "Alagoz");
-	
+
 	cout << *spx << "\n";
 	cout << "use count = " << spx.use_count() << "\n";
 	weak_ptr<Person> wp(spx);
 	cout << "use count = " << spx.use_count() << "\n";
 	cout << "wp.use_count() = " << wp.use_count() << "\n";
-	
+
 	spx.reset();
 
 	if (wp.expired()) {
@@ -26,5 +24,5 @@ int main()
 		shared_ptr sx{ wp };
 		cout << *sx << "\n";
 	}
-	getchar();
+	(void)getchar();
 }
