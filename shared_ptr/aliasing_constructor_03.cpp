7e5691a9ebcac4af197c0184e82a0ff0a6ed7331
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-02-04 13:54:08 +0300

    Update aliasing_constructor_03.cpp

--- shared_ptr/aliasing_constructor_03.cpp
+++ shared_ptr/aliasing_constructor_03.cpp
@@ -24,33 +24,34 @@ using Ovec = std::vector<Owner>;
 
 int main()
 {
-	shared_ptr<Ovec> os(new Ovec); // use_count == 1
-	os->push_back(Owner());
-	os->push_back(Owner());
+	shared_ptr<Ovec> svecptr(new Ovec); // use_count == 1
+	svecptr->reserve(10);
 
-	cout << "[1] os.use_count() = " << os.use_count() << "\n";
+	svecptr->emplace_back();
+	svecptr->emplace_back();
 
-	shared_ptr<Owner> o1(os, &os->at(0));
-	cout << "[2] os.use_count() = " << os.use_count() << "\n";
-	cout << "[3] o1.use_count() = " << o1.use_count() << "\n";
-	shared_ptr<Owner> o2(os, &os->at(1));
-	cout << "[4] os.use_count() = " << os.use_count() << "\n";
-	cout << "[5] o1.use_count() = " << o1.use_count() << "\n";
-	cout << "[6] o2.use_count() = " << o2.use_count() << "\n";
-	shared_ptr<Member> o3(os, &os->at(1).mx); // use_count == 3
+	cout << "[1] svecptr.use_count() = " << svecptr.use_count() << "\n";
 
-	cout << "[7] os.use_count() = " << os.use_count() << "\n";
-	cout << "[8] o1.use_count() = " << o1.use_count() << "\n";
-	cout << "[9] o2.use_count() = " << o2.use_count() << "\n";
-	cout << "[10] o3.use_count() = " << o3.use_count() << "\n";
+	shared_ptr<Owner> optr1(svecptr, &svecptr->at(0));
+	cout << "[2] svecptr.use_count() = " << svecptr.use_count() << "\n";
+	cout << "[3] optr1.use_count() = " << optr1.use_count() << "\n";
+	shared_ptr<Owner> optr2(svecptr, &svecptr->at(1));
+	cout << "[4] svecptr.use_count() = " << svecptr.use_count() << "\n";
+	cout << "[5] optr1.use_count() = " << optr1.use_count() << "\n";
+	cout << "[6] optr2.use_count() = " << optr2.use_count() << "\n";
 
-	os.reset();
+	shared_ptr<Member> smptr(svecptr, &svecptr->at(1).mx); 
 
-	cout << "[11] os.use_count() = " << os.use_count() << "\n";
-	cout << "[12] o1.use_count() = " << o1.use_count() << "\n";
-	cout << "[13] o2.use_count() = " << o2.use_count() << "\n";
-	cout << "[14] o3.use_count() = " << o3.use_count() << "\n";
-	cout << "bir tusa basin ";
+	cout << "[7] svecptr.use_count() = " << svecptr.use_count() << "\n";
+	cout << "[8] optr1.use_count() = " << optr1.use_count() << "\n";
+	cout << "[9] optr2.use_count() = " << optr2.use_count() << "\n";
+	cout << "[10] smptr.use_count() = " << smptr.use_count() << "\n";
+
+	svecptr.reset();
+
+	cout << "[11] svecptr.use_count() = " << svecptr.use_count() << "\n";
+	cout << "[12] optr1.use_count() = " << optr1.use_count() << "\n";
+	cout << "[13] optr2.use_count() = " << optr2.use_count() << "\n";
+	cout << "[14] smptr.use_count() = " << smptr.use_count() << "\n";
 	(void)getchar();
-
 }
