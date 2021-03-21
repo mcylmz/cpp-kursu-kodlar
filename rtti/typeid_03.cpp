61995f89bd6fbac001eae40cba2e171f379f74bf
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-03-21 15:01:38 +0300

    Create typeid_03.cpp

---
+++ rtti/typeid_03.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+class Base {
+public:
+	//virtual ~Base() {}
+	//yorum satırından çıkartıp tekrar derleyip çalıştırın
+};
+
+
+class Der : public Base {
+
+};
+
+
+int main()
+{
+	Der der;
+	Base* ptr{ &der };
+	
+	std::cout << std::boolalpha << (typeid(*ptr) == typeid(Der)) << "\n";
+}
