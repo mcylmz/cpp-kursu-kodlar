00c2d9f4f9348a4e719eb476743bff8f3ba23577
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-06-04 17:36:09 +0300

    Create moving_argument_01.cpp

---
+++ concurrency/thread/moving_argument_01.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <iostream>
+#include <thread>
+
+void func(std::vector<int>&& vec)
+{
+	for (auto i : vec)
+		std::cout << i << " ";
+	std::cout << "\n";
+}
+
+int main()
+{
+	using namespace std;
+
+	vector<int> ivec{ 1, 2, 3, 4, 5, 6, 7 };
+	thread tx{ func, move(ivec) }; //moving
+	tx.join();
+	cout << "ivec.size() = " << ivec.size() << "\n";
+}
