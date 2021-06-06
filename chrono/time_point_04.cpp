5d05794e93d347615c1b1c8aa4954175622a341c
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-06-06 05:41:29 +0300

    Update time_point_04.cpp

--- chrono/time_point_04.cpp
+++ chrono/time_point_04.cpp
@@ -1,46 +1,9 @@
 #include <chrono>
-#include <iostream>
-#include <iomanip>
 #include <random>
 
 using namespace std;
 using namespace chrono;
 
-
-void print_time()
-{
-	static const char* const pmons[] = {
-	"Ocak",
-	"Subat",
-	"Mart",
-	"Nisan",
-	"Mayis",
-	"Haziran",
-	"Temmuz",
-	"Agustos",
-	"Eylul",
-	"Ekim",
-	"Kasim",
-	"Aralik"
-	};
-
-	static const char* const pdays[] = {
-	"Pazar",
-	"Pazartesi",
-	"Sali",
-	"Carsamba",
-	"Persembe",
-	"Cuma",
-	"Cumartesi",
-	};
-
-	time_t timer = system_clock::to_time_t(system_clock::now());
-	//cout << ctime(&timer) << "\n";
-	auto p = localtime(&timer);
-	cout.fill('0');
-	cout << setw(2) << p->tm_mday << " " << pmons[p->tm_mon] << " " << p->tm_year + 1900 << " " <<
-		pdays[p->tm_wday] << " " << setw(2) << p->tm_hour << ":" << setw(2) << p->tm_min << ":" << setw(2) << p->tm_sec << "\n";
-}
 
 int main()
 {
