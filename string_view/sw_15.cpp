05b3ad8dce051dea018e55285a46a74ee384f9fe
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-06-27 08:38:38 +0300

    Create sw_15.cpp

---
+++ string_view/sw_15.cpp
@@ -0,0 +1,13 @@
+#include <cstring>
+#include <iostream>
+#include <string_view>
+
+int main()
+{
+	char s[]{ 'n', 'e', 'c', 'o' };
+
+	std::string_view sv{ s, 4};
+
+	std::cout << sv << "\n"; //no problem
+	std::cout << sv.data() << "\n"; //ub
+}
