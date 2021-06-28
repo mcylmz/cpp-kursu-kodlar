b434b4f80a221a26c95de4ca0f4e4b5735dc0549
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-06-28 16:47:51 +0300

    Create mutex_04.cpp

---
+++ concurrency/mutex/mutex_04.cpp
@@ -0,0 +1,33 @@
+#include <sstream>
+#include <mutex>
+#include <iostream>
+#include <thread>
+#include <vector>
+
+
+struct pcout : std::stringstream {
+public:
+	~pcout()
+	{
+		std::lock_guard locker{ cmutex };
+		std::cout << rdbuf();
+		std::cout.flush();
+	}
+	static inline std::mutex cmutex;
+};
+
+void print(int x)
+{
+	pcout{} << "print islevi x = " << x << "\n";
+}
+
+int main()
+{
+	std::vector<std::thread> tvec;
+
+	for (int i = 0; i < 20; ++i)
+		tvec.emplace_back(std::thread{ print, i });
+
+	for (auto& t : tvec)
+		t.join();
+}
