cb775865da7885bb3a0db2bb7a6d9d1afbb9f7d0
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-06-28 16:55:33 +0300

    Create timed_mutex_01.cpp

---
+++ concurrency/mutex/timed_mutex_01.cpp
@@ -0,0 +1,34 @@
+#include <mutex>
+#include <chrono>
+#include <thread>
+#include <iostream>
+
+
+int gcounter{}; //global değişken
+std::timed_mutex mtx;
+
+void increment(int i)
+{
+	using namespace std::chrono;
+
+
+	if (mtx.try_lock_for(1ms)) {
+		++gcounter;
+		std::this_thread::sleep_for(10ms);
+		std::cout << "thread : " << i << " kritik bolgeye girdi\n";
+		mtx.unlock();
+	}
+	else
+		std::cout << "thread  " << i << " kritik bolgeye giremedi\n";
+}
+
+int main()
+{
+	std::thread t1{ increment, 1 }; 
+	std::thread t2{ increment, 2 };
+
+	t1.join(); 
+	t2.join();
+	std::cout << "gcounter = " << gcounter << "\n";
+}
+
