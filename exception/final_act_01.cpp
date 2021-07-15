7c41cb22907dcb5213ba1912bb776443e07bc4f7
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-07-15 18:25:58 +0300

    Update final_act_01.cpp

--- exception/final_act_01.cpp
+++ exception/final_act_01.cpp
@@ -22,3 +22,16 @@ private:
 	F f_;
 	bool invoke_;
 };
+
+
+template <class F>
+inline final_act<F> finally(const F& f) noexcept
+{
+    return final_act<F>(f);
+}
+
+template <class F>
+inline final_act<F> finally(F&& f) noexcept
+{
+    return final_act<F>(std::forward<F>(f));
+}
