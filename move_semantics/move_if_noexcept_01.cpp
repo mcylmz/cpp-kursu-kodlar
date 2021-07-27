79ba9438bfc0996981053b4e7560a8a6492b58b8
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-07-27 14:59:19 +0300

    Create move_if_noexcept_01.cpp

---
+++ move_semantics/move_if_noexcept_01.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+struct A {
+	A() = default;
+
+	//A(A&&)
+	A(A&&)noexcept
+	{
+		std::cout << "move ctor\n";
+	}
+
+	A(const A&)
+	{
+		std::cout << "copy ctor\n";
+	}
+};
+
+
+int main()
+{
+	A ax;
+
+	A bx(std::move_if_noexcept(ax));
+}
