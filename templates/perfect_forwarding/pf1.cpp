7636fbda8cdbcf5a8afb2839a78c38faef220cd9
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-08-03 12:24:03 +0300

    Create pf1.cpp

---
+++ templates/perfect_forwarding/pf1.cpp
@@ -0,0 +1,16 @@
+//Basit bir forward implementasyonu
+//fonksiyon T ve T& türleri için yazılacak
+
+template<class T>
+T&& forward(typename std::remove_reference<T>::type& t) noexcept 
+{
+	return static_cast<T&&>(t);
+}
+
+template <class T>
+T&& forward(typename std::remove_reference<T>::type&& t) noexcept 
+{
+	return static_cast<T&&>(t);
+}
+
+
