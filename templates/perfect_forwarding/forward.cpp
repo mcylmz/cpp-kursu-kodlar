a45dddf44441a1ae8e38e1cd7d27f2794e40f3b5
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-08-09 02:21:09 +0300

    Update forward.cpp

--- templates/perfect_forwarding/forward.cpp
+++ templates/perfect_forwarding/forward.cpp
@@ -7,10 +7,3 @@ T&& forward(typename std::remove_reference<T>::type& t) noexcept
 	return static_cast<T&&>(t);
 }
 
-template <class T>
-T&& forward(typename std::remove_reference<T>::type&& t) noexcept 
-{
-	return static_cast<T&&>(t);
-}
-
-
