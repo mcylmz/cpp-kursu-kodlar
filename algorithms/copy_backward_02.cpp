e383490b17051e084ebb7cd5b9fbde2e5dce3004
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-08-30 11:58:27 +0300

    Update copy_backward_02.cpp

--- algorithms/copy_backward_02.cpp
+++ algorithms/copy_backward_02.cpp
@@ -1,1 +1,16 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
 
+
+using namespace std;
+
+int main()
+{
+	vector<int> ivec{ 10, 20, 30, 40, 50 };
+	vector<int> dest(ivec.size() * 2);
+
+	copy_backward(ivec.begin(), ivec.end(), dest.end());
+	for (auto i : dest)
+		std::cout << i << " ";
+}
