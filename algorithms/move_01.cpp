da61b2f510f5cf49ddf7ebf5bb3d5a9ffe0a0a1f
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-08-30 12:07:48 +0300

    Create move_01.cpp

---
+++ algorithms/move_01.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Myclass {
+public:
+	Myclass() = default;
+
+	Myclass& operator=(const Myclass&) 
+	{
+		std::cout << "copy assignment\n";
+		return *this;
+	}
+
+	Myclass& operator=(Myclass &&)
+	{
+		std::cout << "move assignment\n";
+		return *this;
+	}
+};
+
+int main()
+{
+	std::vector<Myclass> vec_source(5);
+	std::vector<Myclass> vec_dest(5);
+
+	copy(vec_source.begin(), vec_source.end(), vec_dest.begin());
+	move(vec_source.begin(), vec_source.end(), vec_dest.begin());
+	
+}
