f83690127424feffd631440c6e640c82b1467297
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-09-05 11:08:09 +0300

    Update reference__wrapper_08.cpp

--- functional/reference_wrapper/reference__wrapper_08.cpp
+++ functional/reference_wrapper/reference__wrapper_08.cpp
@@ -4,6 +4,7 @@
 int main()
 {
 	using namespace std;
-	reference_wrapper r = "ali"s; //gecersiz
+	//reference_wrapper r1 = "ali"s; //gecersiz
+	reference_wrapper<const string> r2 = "ali"s; //gecersiz
 	//reference_wrapper R value ifadesine bağlanamaz
 }
