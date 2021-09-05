ee6ad3e8ca5978145745f73c25a77a743514d54c
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-09-05 12:01:54 +0300

    Update reference__wrapper_11.cpp

--- functional/reference_wrapper/reference__wrapper_11.cpp
+++ functional/reference_wrapper/reference__wrapper_11.cpp
@@ -1,1 +1,29 @@
+#include <functional>
+
+constexpr int foo(int x)
+{
+	return 2 * x - 3;
+}
+
+constexpr int bar(int x)
+{
+	return 3 * x + 7;
+}
+
+constexpr int baz(bool b)
+{
+	std::reference_wrapper r = foo; //C++20
+	if (b)
+		r = bar;
+
+	return r(10);
+}
+
+int main()
+{
+	constexpr auto x = baz(true);
+	constexpr auto y = baz(false);
+}
+
+
 
