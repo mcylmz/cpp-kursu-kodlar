d0eb2f3d54e057695591f9fb0d04af55ab238d06
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-09-22 15:30:11 +0300

    Create perfect_forwarding_return value.cpp

---
+++ templates/perfect_forwarding/perfect_forwarding_return value.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+class Nec {};
+
+void f(Nec&)
+{
+	std::cout << "Nec&\n";
+}
+
+void f(const Nec&)
+{
+	std::cout << "const Nec&\n";
+}
+
+void f(Nec&&)
+{
+	std::cout << "Nec&&\n";
+}
+
+void f(const Nec&&)
+{
+	std::cout << "const Nec&&\n";
+}
+
+Nec& foo_ret_lref(Nec& r)
+{
+	return r;
+}
+
+const Nec& foo_ret_const_lref(const Nec& r)
+{
+	return r;
+}
+
+Nec&& foo_ret_rref(Nec&& r)
+{
+	return std::move(r);
+}
+
+const Nec&& foo_ret_const_rref(const Nec&& r)
+{
+	return std::move(r);
+}
+
+int main()
+{
+	Nec nx;
+	Nec cnec;
+
+	f(foo_ret_lref(nx));
+	f(foo_ret_const_lref(nx));
+	f(foo_ret_rref(Nec{}));
+	f(foo_ret_const_rref(std::move(cnec)));
+}
