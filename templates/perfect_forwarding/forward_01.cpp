ea3b83d614bd199945ff26afea96fbbe7b9dff1f
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-09-24 23:17:10 +0300

    Update forward_01.cpp

--- templates/perfect_forwarding/forward_01.cpp
+++ templates/perfect_forwarding/forward_01.cpp
@@ -1,8 +1,7 @@
 //Basit bir forward implementasyonu
-//fonksiyon T ve T& türleri için yazılacak
 
 template<class T>
-T&& forward(typename std::remove_reference<T>::type& t) noexcept 
+constexpr T&& forward(typename std::remove_reference<T>::type& t) noexcept 
 {
 	return static_cast<T&&>(t);
 }
