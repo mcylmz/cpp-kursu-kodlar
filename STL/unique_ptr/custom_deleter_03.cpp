9b44573d128ea342865d2f2e5d94d659bfc11893
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-09-25 09:16:48 +0300

    Update custom_deleter_03.cpp

--- STL/unique_ptr/custom_deleter_03.cpp
+++ STL/unique_ptr/custom_deleter_03.cpp
@@ -5,12 +5,11 @@ void mydeleter(std::string* p)
 {
 	std::cout << "delete edilen nesne adresi: " << p << "\n";
 	delete p;
-
 };
 
 int main()
 {
 	using namespace std;
 
-	unique_ptr<string, decltype(&mydeleter)> uptr{ new string{"ahmet aksoy"}, mydeleter };
+	unique_ptr<string, decltype(&mydeleter)> uptr{ new string{"necati ergin"}, mydeleter };
 }
