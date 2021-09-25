e99f94cbbdeb6f17001b9b0a5c1e9d2e6ec423b5
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-09-25 09:11:22 +0300

    Update address_inserter_01.cpp

--- STL/unique_ptr/address_inserter_01.cpp
+++ STL/unique_ptr/address_inserter_01.cpp
@@ -2,15 +2,13 @@
 #include "triple.h"
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
-	std::cout << "main basladi\n";
+	using namespace std;
+	cout << "main basladi\n";
 	Triple* ptr = new Triple{ 1, 2, 3 };
-	std::cout << "ptr = " << ptr << "\n";
+	cout << "ptr = " << ptr << "\n";
 	unique_ptr<Triple> up{ ptr };
 	cout << up << "\n";
 	cout << up.get() << "\n";
 }
-
