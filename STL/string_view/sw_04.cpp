c5fa0a902b4f21dedde9c8978bd13f02d954a7d8
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-09-25 11:56:12 +0300

    Update sw_04.cpp

--- STL/string_view/sw_04.cpp
+++ STL/string_view/sw_04.cpp
@@ -1,15 +1,18 @@
 #include <string>
 #include <string_view>
+#include <iostream>
+
 
 int main()
 {
-	using namespace std;
-
-	string_view sv;
-	string str{ "necati" };
+	std::string_view sv;
+	std::string str{ "necati" };
 
 	sv = str;
 	sv = str.operator std::basic_string_view<char, std::char_traits<char>>();
+
+	std::cout << "sv.length() = " << sv.length() << "\n";
+	std::cout << "sv = " << sv << "\n";
 }
 
 /*
