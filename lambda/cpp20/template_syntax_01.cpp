687ba416f9d90dedac3b20d8ed967ca8887a6919
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-09-25 12:58:11 +0300

    Create template_syntax_01.cpp

---
+++ lambda/cpp20/template_syntax_01.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+template <typename ...Args>
+void foo(Args && ...args);
+
+
+int main()
+{
+	auto fpush = []<typename T>(std::vector<T> &x, const T & val)
+	{
+		x.push_back(val);
+	};
+
+	auto call_foo = []<typename ...Args>(Args && ...args) { foo(std::forward<Args>(args)...); };
+}
