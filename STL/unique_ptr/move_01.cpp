e4d4cbdff9d6ebf432ac2be648802ecab6dded6b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-09-25 09:36:34 +0300

    Update move_01.cpp

--- STL/unique_ptr/move_01.cpp
+++ STL/unique_ptr/move_01.cpp
@@ -3,15 +3,14 @@
 
 int main()
 {
-	using namespace std;
-	auto upx = make_unique<Triple>(10, 30, 56);
+	auto upx = std::make_unique<Triple>(10, 30, 56);
 
-	cout << "upx = " << (upx ? "dolu" : "bos") << "\n";
+	std::cout << "upx = " << (upx ? "dolu" : "bos") << "\n";
 
 	auto upy = move(upx);
-	getchar();
 
-	cout << "upx = " << (upx ? "dolu" : "bos") << "\n";
-	cout << "upy = " << (upy ? "dolu" : "bos") << "\n";
+	(void)getchar();
+
+	std::cout << "upx = " << (upx ? "dolu" : "bos") << "\n";
+	std::cout << "upy = " << (upy ? "dolu" : "bos") << "\n";
 }
-
