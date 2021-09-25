8eaa6fffc99640a134a9eb40017e1a722577341c
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-09-25 09:35:33 +0300

    Update make_unique_01.cpp

--- STL/unique_ptr/make_unique_01.cpp
+++ STL/unique_ptr/make_unique_01.cpp
@@ -10,12 +10,10 @@ std::unique_ptr<T> MakeUnique(Args && ...args)
 
 int main()
 {
-	using namespace std;
-
 	std::cout << "main basladi\n";
 	auto uptr = MakeUnique<Triple>(10, 20, 30);
 
-	cout << *uptr << "\n";
+	std::cout << *uptr << "\n";
 	uptr->set(3, 4, 7);
-	cout << *uptr << "\n";
+	std::cout << *uptr << "\n";
 }
