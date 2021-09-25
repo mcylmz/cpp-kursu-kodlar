6f3634ebf7b7d0e4e82941ae5df52078f0083d7a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-09-25 09:22:12 +0300

    Update error_03.cpp

--- STL/unique_ptr/error_03.cpp
+++ STL/unique_ptr/error_03.cpp
@@ -3,12 +3,11 @@
 
 int main()
 {
-	using namespace std;
-
-	auto upx = make_unique<Triple>(1, 2, 3);
-	cout << "upx = " << (upx ? "dolu" : "bos") << "\n";
+	auto upx = std::make_unique<Triple>(1, 2, 3);
+	std::cout << "upx = " << (upx ? "dolu" : "bos") << "\n";
 	auto pt = upx.release();
-	cout << "upx = " << (upx ? "dolu" : "bos") << "\n";
-	cout << *pt << "\n";
-	cout << *upx << "\n"; //ub
+	std::cout << "upx = " << (upx ? "dolu" : "bos") << "\n";
+	std::cout << *pt << "\n";
+	std::cout << std::boolalpha << (upx == nullptr) << "\n"; 
+	std::cout << *upx << "\n"; //ub
 }
