c54af5f2d2f44c274a0bd722d06b6e1cc4c5045e
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-09-25 09:23:28 +0300

    Update error_05.cpp

--- STL/unique_ptr/error_05.cpp
+++ STL/unique_ptr/error_05.cpp
@@ -3,9 +3,8 @@
 
 int main()
 {
-	using namespace std;
-	auto upx = make_unique<Triple>(1, 4, 7);
+	auto upx = std::make_unique<Triple>(1, 4, 7);
 	Triple* ptr = upx.get();
-	
-	unique_ptr<Triple> upy(ptr); //ub
+
+	std::unique_ptr<Triple> upy(ptr); //ub
 }
