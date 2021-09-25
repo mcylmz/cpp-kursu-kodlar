bbee69042a0769b0d0740de900ce0c6c8d9ae318
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-09-25 12:25:54 +0300

    Update and rename sw_19.cpp to remove_prefix2.cpp

--- STL/string_view/remove_prefix2.cpp
+++ STL/string_view/remove_prefix2.cpp
@@ -10,6 +10,7 @@ int main()
 	str.remove_suffix(4);
 
 	std::cout << "the length of " << str << " is " << std::strlen(str.data()) << "\n";
+	std::cout << "the length of " << str << " is " << str.length() << "\n";
 	std::cout << "str.data() is " << str.data() << '\n';
 	std::cout << "str is [" << str << "]\n";
 }
