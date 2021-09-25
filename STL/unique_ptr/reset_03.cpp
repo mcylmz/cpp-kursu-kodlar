829085da663f041e56a0135ea465f0afa7eae830
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-09-25 09:42:07 +0300

    Update reset_03.cpp

--- STL/unique_ptr/reset_03.cpp
+++ STL/unique_ptr/reset_03.cpp
@@ -3,13 +3,11 @@
 
 int main()
 {
-	using namespace std;
-
-	auto upx = make_unique<Triple>(10, 30, 56);
-	cout << *upx << "\n";
-	cout << (upx ? "dolu" : "bos") << "\n";
+	auto upx = std::make_unique<Triple>(10, 30, 56);
+	std::cout << *upx << "\n";
+	std::cout << (upx ? "dolu" : "bos") << "\n";
 	upx.reset();
 	//upx.reset(nullptr);
-	cout << (upx ? "dolu" : "bos") << "\n";
+	std::cout << (upx ? "dolu" : "bos") << "\n";
 	std::cout << "main devam ediyor\n";
 }
