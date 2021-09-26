b61e7a715d350edbff2f084377bbe342d20bba65
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-09-26 10:54:08 +0300

    Create soru01.cpp

---
+++ lambda/sorular/soru01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int g = 99;
+
+auto fx = [=] {return g + 1; };
+auto fy = [g = g] {return g + 1; };
+
+int main()
+{
+	g = 500;
+
+	std::cout << fx() << "\n";
+	std::cout << fy() << "\n";
+}
