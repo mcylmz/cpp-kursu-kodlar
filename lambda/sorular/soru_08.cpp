000081b9d482016f3996e2775d09216e5f0124f1
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-09-27 11:01:42 +0300

    Create soru_08.cpp

---
+++ lambda/sorular/soru_08.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int x = 4;
+    auto y = [x = x + 1, &r = x]()
+    {
+        r += 2;
+        return x * x;
+    }(); 
+
+    std::cout << "y = " << y << "\n";
+    std::cout << "x = " << x << "\n";
+}
