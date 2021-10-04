079321d43eadd508defd746bf8009e90ae3db961
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-10-04 13:13:46 +0300

    Update init_pack_07.cpp

--- lambda/cpp20/init_pack_expansion/init_pack_07.cpp
+++ lambda/cpp20/init_pack_expansion/init_pack_07.cpp
@@ -1,8 +1,9 @@
+#include <functional>
 #include <utility>
 
 template <class... Args>
 auto delay_invoke_foo(Args... args) {
-    return[args = std::move(args)...]() -> decltype(auto) {
+    return[...args = std::move(args)]() -> decltype(auto) {
         return foo(args...);
     };
 }
