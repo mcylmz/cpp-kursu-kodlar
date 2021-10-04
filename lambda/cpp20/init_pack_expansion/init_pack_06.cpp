3af358a5164102c669695a68d9c571bee7a5f77e
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-10-04 11:26:12 +0300

    Create init_pack_06.cpp

---
+++ lambda/cpp20/init_pack_expansion/init_pack_06.cpp
@@ -0,0 +1,8 @@
+template <class... Args>
+auto delay_invoke_foo(Args... args) {
+    return[tup = std::make_tuple(std::move(args)...)]() -> decltype(auto) {
+        return std::apply([](auto const&... args) -> decltype(auto) {
+            return foo(args...);
+            }, tup);
+    };
+}
