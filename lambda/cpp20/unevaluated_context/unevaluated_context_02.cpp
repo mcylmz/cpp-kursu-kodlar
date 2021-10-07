e19affe84f46bd8e2b32658eaf60cc46d9c6aad6
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-10-08 01:23:58 +0300

    Update unevaluated_context_02.cpp

--- lambda/cpp20/unevaluated_context/unevaluated_context_02.cpp
+++ lambda/cpp20/unevaluated_context/unevaluated_context_02.cpp
@@ -2,36 +2,34 @@
 #include <string>
 #include <iostream>
 
-
 template <typename C>
-void print(const C& Con) 
+void print(const C& con)
 {
-    for (const auto& elem : Con) 
+    for (const auto& elem : con)
         std::cout << elem << "  ";
-    
+
     std::cout << '\n';
 }
 
-int main() 
+int main()
 {
     using namespace std;
 
-    set<string> names = { "Ayse", "Zehra", "Murat", "Bilal", "Sumeyye", "Nur", "Cemil"};
+    set<string> names = { "Ayse", "Zehra", "Murat", "Bilal", "Sumeyye", "Nur", "Cemil" };
     print(names);
 
-    using gset = set<string, decltype([](const auto& l, const auto& r) { return l > r; })>;
+    using gset = set <string, decltype([](const auto& l, const auto& r) { return l > r; }) > ;
     gset gnames = { "Ayse", "Zehra", "Murat", "Bilal", "Sumeyye", "Nur", "Cemil" };
     print(gnames);
 
-    using lenset = set<string, decltype([](const auto& l, const auto& r) { return l.size() < r.size(); }) > ; 
+    using lenset = set < string, decltype([](const auto& l, const auto& r) { return l.size() < r.size(); }) > ;
     lenset namelens = { "Ayse", "Zehra", "Murat", "Bilal", "Sumeyye", "Nur", "Cemil" };
     print(namelens);
 
-    set<int> iset = { 9, -3, 12, -5, 40, -8, 77, -75};
+    set<int> iset = { 9, -3, 12, -5, 40, -8, 77, -75 };
     print(iset);
 
-    using abs_set = set<int, decltype([](const auto& l, const auto& r) { return  abs(l) < abs(r); })> ; 
+    using abs_set = set<int, decltype([](const auto& l, const auto& r) { return  abs(l) < abs(r); }) > ;
     abs_set abs_vals = { 9, -3, 12, -5, 40, -8, 77, -75 };
-    print(abs_vals);    
-
+    print(abs_vals);
 }
