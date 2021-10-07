1d51c0f0fa39d0631572a618fde9da67630616ef
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-10-08 01:26:26 +0300

    Update templated_lambda_01.cpp

--- lambda/cpp20/templated_lambda/templated_lambda_01.cpp
+++ lambda/cpp20/templated_lambda/templated_lambda_01.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
 	auto f = []<typename T>(T x, T y) { return x + y; };
 
-	cout << f(34, 39) << "\n";
-	cout << f(.22, .34) << "\n";
-	//cout << f(55, .34) << "\n";  //gecersiz
+	std::cout << f(34, 39) << "\n";
+	std::cout << f(.22, .34) << "\n";
+	//std::cout << f(55, .34) << "\n";  //gecersiz
 }
