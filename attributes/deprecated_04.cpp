7ce34ca3bd2bb95779f3da283b55afc5205a2d87
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-10-14 13:57:01 +0300

    Create deprecated_04.cpp

---
+++ attributes/deprecated_04.cpp
@@ -0,0 +1,21 @@
+//[[deprecated]] attribute extended in C++17
+
+enum Color {
+	gray = 0,
+	grey [[deprecated]] = gray
+};
+
+Color c = grey; //compiler diagnostic
+
+namespace [[deprecated]] old_version{
+	void legacy_func();
+}
+
+
+int main()
+{
+	old_version::legacy_func(); //compiler diagnostic
+}
+© 2021 GitHub, Inc.
+Terms
+Priv
