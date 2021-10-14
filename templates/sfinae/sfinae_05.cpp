2c7b600a7c66085b8eae266cde011c90fbfe0ba0
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-10-14 13:51:15 +0300

    Create sfinae_05.cpp

---
+++ templates/sfinae/sfinae_05.cpp
@@ -0,0 +1,10 @@
+#include <type_traits>
+#include <iostream>
+
+template <class T>
+typename std::enable_if_t<std::is_arithmetic_v<T>, T>
+func(T t)
+{
+	std::cout << "func<arithmetic T>\n";
+	return t;
+}
