f53236622064e8d7eac53593f87b73214711a97c
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-10-18 16:25:56 +0300

    Update cooked_02.cpp

--- user_defined_literals/cooked_02.cpp
+++ user_defined_literals/cooked_02.cpp
@@ -1,16 +1,17 @@
 //cooked usef defined literals
 
+constexpr double operator"" _c(long double fdegrees)
+{
+	return static_cast<double>(fdegrees);
+}
+
 constexpr double operator"" _fag(long double fdegrees)
 {
 	return static_cast<double>((fdegrees - 32) / 1.8);
 }
 
-#include <iostream>
-
-using namespace std;
-
-
 int main()
 {
-	std::cout << 34.87_fag;
+	constexpr auto val = 50._fag - 15._c;
+	constexpr auto x = operator""_fag(50.) - operator""_c(15.);
 }
