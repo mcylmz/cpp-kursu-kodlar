a188adc62640382d6e149b6f69a29400f443422f
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-10-18 12:04:17 +0300

    Update dangling_pointer_05.cpp

--- STL/string_view/dangling_pointer_05.cpp
+++ STL/string_view/dangling_pointer_05.cpp
@@ -1,17 +1,15 @@
 #include <string_view>
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
-	string str{ "necati ergin" };
+	std::string str{ "necati ergin" };
 
-	string_view sw{ str };
+	std::string_view sw{ str };
 	std::cout << "[" << sw << "]\n";
 	str.front() = 'X';
 	str.back() = 'X';
 	std::cout << "[" << sw << "]\n";
 	str.append(500, 'A');
-	std::cout << "[" << sw << "]\n"; //ub
+	std::cout << "[" << sw << "]\n"; 
 }
