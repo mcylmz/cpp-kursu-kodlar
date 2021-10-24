40b35d01581f583bb4a5f640a52d9cb1c3e6b41c
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-10-24 21:08:18 +0300

    Update deduction_015.cpp

--- templates/deduction/deduction_015.cpp
+++ templates/deduction/deduction_015.cpp
@@ -5,8 +5,8 @@ void foo(std::array<T, sizeof(U)>, std::array<U, sizeof(T)>);
 
 int main()
 {
-	std::array<int, 8> a;
-	std::array<double, 4> b;
+    std::array<int, 8> a;
+    std::array<double, 4> b;
 	std::array<double, 5> c;
 
 	foo(a, b);
