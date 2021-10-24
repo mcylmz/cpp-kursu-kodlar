4feb5bfa7a86246f995894f1cb3a141db4414755
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-10-24 13:04:34 +0300

    Update discrete_dist_01.cpp

--- STL/random/discrete_dist_01.cpp
+++ STL/random/discrete_dist_01.cpp
@@ -1,3 +1,5 @@
+//std::discreet_distribution
+
 #include <iostream>
 #include <random>
 #include <map>
@@ -10,11 +12,11 @@ int main()
 	std::array<double, 6> weights{ 10.0, 10.0, 10.0, 10.0, 10.0, 12.0 };
 
 	std::discrete_distribution<size_t> dist{ std::begin(weights), std::end(weights) };
-	std::map<size_t, size_t> results; 
+	std::map<size_t, size_t> results;
 
-	for (size_t i{}; i < 10'000; ++i) 
+	for (size_t i{}; i < 10'000; ++i)
 		++results[dist(eng)];
 
 	for (const auto& [die, count] : results)
-		std::cout << "zar degeri " << die << " : " << count << "\n";
+		std::cout << "zar degeri " << die + 1 << " : " << count << "\n";
 }
