c40ad78a03208354427ffa0619a09dd92d4c88c7
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-10-27 18:42:23 +0300

    Create basic_01.cpp

---
+++ templates/sfinae/basic_01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+template<typename T, unsigned N>
+std::size_t len(T(&)[N])
+{
+	return N;
+}
+
+template<typename T>
+typename T::size_type len(T const& t)
+{
+	return t.size();
+}
+
+int main()
+{
+	int a[10]{};
+	std::cout << len(a); 
+	std::cout << len("tmp"); 
+}
