3c68d22f43d9580901e87173d32b72c3f1544948
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-10-29 16:01:50 +0300

    Create partial_specialization_01.cpp

---
+++ templates/specialization/partial_specialization_01.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+template <typename T>
+struct Myclass {
+	Myclass()
+	{
+		std::cout << "primary template\n";
+	}
+};
+
+
+template <typename T>
+struct Myclass<T*> {
+	Myclass()
+	{
+		std::cout << "Myclass<T*> specialization>\n";
+	}
+};
+
+int main()
+{
+	Myclass<int> m1;
+	Myclass<double> m2;
+	Myclass<int *> m3;
+	Myclass<float *> m4;
+}
