397c84969dca1c01be57ec7c314d5438d51114cc
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-10-30 12:16:30 +0300

    Create da_01.cpp

---
+++ templates/default_arguments/da_01.cpp
@@ -0,0 +1,15 @@
+#include <memory>
+
+template <typename T, typename U = int>
+class A {};
+
+template <typename T, typename U = T>
+class B {};
+
+
+int main()
+{
+	A<double> x; //A<double, int>
+	A<double, double> y;
+	B<int> z; //B<int, int> z;
+}
