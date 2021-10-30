bb36a47d5107b8bbc92d4122c52aea20c3b724c7
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-10-30 11:27:13 +0300

    Update non_type_06.cpp

--- templates/non_type_parameters/non_type_06.cpp
+++ templates/non_type_parameters/non_type_06.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
 
-template<typename T, int N, int M> 
-bool less(T(&a)[N], T(&b)[M])
+template<typename T, int N, int M>
+constexpr bool less(const T(&a)[N], const T(&b)[M])
 {
 	for (int i = 0; i < N && i < M; ++i)
 	{
-		if (a[i] < b[i]) 
-			return true; 
-	
-		if (b[i] < a[i]) 
+		if (a[i] < b[i])
+			return true;
+
+		if (b[i] < a[i])
 			return false;
-	} 
-	
+	}
+
 	return N < M;
 }
 
 int main()
 {
 	int a[] = { 3, 7, 9 };
-	int b[] = { 3, 7, 9, 2, 6 };
-	int c[] = { 3, 7, 9, 3, 6 };
+	constexpr int b[] = { 3, 7, 9, 2, 6 };
+	constexpr int c[] = { 3, 7, 9, 3, 6 };
 
-	std::cout << std::boolalpha << less(a, b) << '\n';
-	std::cout << std::boolalpha << less(c, b) << '\n';
+	constexpr auto f = less(b, c);
+
+	std::cout << std::boolalpha << (a < b);
 }
