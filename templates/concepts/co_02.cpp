f6273295e3f547bb8863404d2166faeabaff9d7d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-10-31 11:36:05 +0300

    Create co_02.cpp

---
+++ templates/concepts/co_02.cpp
@@ -0,0 +1,46 @@
+#include <concepts>
+#include <iostream>
+
+template <typename T> requires std::integral<T>
+T gcd1(T a, T b) 
+{
+    if (b == 0) 
+        return a;
+    
+    return gcd1(b, a % b);
+}
+
+
+template<std::integral T>
+T gcd2(T a, T b) 
+{
+    if (b == 0) 
+        return a;
+    
+    return gcd2(b, a % b);
+}
+
+std::integral auto gcd3(std::integral auto a, std::integral auto b) 
+{
+    if (b == 0) 
+        return a;
+    
+    return gcd3(b, a % b);
+}
+
+auto gcd4(auto a, auto b) 
+{
+    if (b == 0) 
+        return a;
+    
+    return gcd4(b, a % b);
+}
+
+int main() 
+{
+
+    std::cout << "gcd1(40, 28)= " << gcd1(40, 28) << '\n';
+    std::cout << "gcd2(40, 28)= " << gcd2(40, 28) << '\n';
+    std::cout << "gcd3(40, 28)= " << gcd3(40, 28) << '\n';
+    std::cout << "gcd4(40, 28)= " << gcd4(40, 28) << '\n';
+}
