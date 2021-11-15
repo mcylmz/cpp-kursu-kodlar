8a6b3547405f78b9ec3dfad66041db2562a92e2e
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-11-15 22:51:04 +0300

    Update dg_08.cpp

--- templates/deduction_guides/dg_08.cpp
+++ templates/deduction_guides/dg_08.cpp
@@ -6,5 +6,5 @@ struct pair {
 template<typename T1, typename T2>
 pair(T1, T2)->pair<T1, T2>;			// deduce argument types by-value
 
-pair px{ "necati", "ergin" }; // const char[3] and const char[6]
+pair px{ "necati", "ergin" }; // const char[7] and const char[6]
 pair<const char*, const char*> py{ "necati", "ergin" };
