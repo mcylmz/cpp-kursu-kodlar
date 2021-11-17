cfbb032878d2f6fbe13fd89242abd8e63abde5c7
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-11-17 16:54:01 +0300

    Update singleton.cpp

--- templates/crtp/singleton.cpp
+++ templates/crtp/singleton.cpp
@@ -8,15 +8,13 @@ class Singleton
 {
     static std::unique_ptr<TDerived> m_instance;
     static std::once_flag            m_once;
-
 protected:
-
     Singleton() {}
-
 public:
 
     ~Singleton() { }
-
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
     static TDerived& GetInstance()
     {
         std::call_once
@@ -33,3 +31,28 @@ std::unique_ptr<TDerived>  Singleton<TDerived>::m_instance;
 
 template<typename TDerived>
 std::once_flag   Singleton<TDerived>::m_once;
+
+
+#include <iostream>
+
+class Myclass : public Singleton<Myclass>
+{
+public:
+    Myclass() {
+        std::cout << "Myclass ctor\n";
+    }
+
+    ~Myclass() {
+        std::cout << "Myclass destructor\n";
+    }
+
+    void foo()
+    {
+        std::cout << "Myclass::foo()\n";
+    }
+};
+
+int main()
+{
+    Myclass::GetInstance().foo();
+}
