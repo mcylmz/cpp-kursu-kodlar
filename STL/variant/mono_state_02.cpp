56e0b8228c8a7a20674de8c0ded42e7f32babf33
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-11-29 13:16:49 +0300

    Create mono_state_02.cpp

---
+++ STL/variant/mono_state_02.cpp
@@ -0,0 +1,30 @@
+#include <variant>
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::variant<std::monostate, std::string, int, double> vx;
+
+	if (std::holds_alternative<std::monostate>(vx))
+		std::cout << "variant bos (monostate)\n";
+	else
+		std::cout << "variant bos degil\n";
+
+	if (std::get_if<std::monostate>(&vx))
+		std::cout << "variant bos (monostate)\n";
+	else
+		std::cout << "variant bos degil\n";
+
+
+	if (vx.index() == 0)
+		std::cout << "variant bos (monostate)\n";
+	else
+		std::cout << "variant bos degil\n";
+
+	if (!vx.index())
+		std::cout << "variant bos (monostate)\n";
+	else
+		std::cout << "variant bos degil\n";
+
+}
