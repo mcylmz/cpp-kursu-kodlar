dd31588db31cf21c1db0a3f7660b8031230db144
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-11-29 14:38:59 +0300

    Create in_place_index.cpp

---
+++ STL/variant/in_place_index.cpp
@@ -0,0 +1,18 @@
+#include <variant>
+#include <iostream>
+#include <set>
+
+int main()
+{
+	auto pred = [](int x, int y) {
+		return std::abs(x) < std::abs(y);
+	};
+	
+	std::variant<
+		std::set<int>, 
+		std::set<int, decltype(pred)>> vx{ 
+		std::in_place_index<1>,
+		{4, 8, -7, -2, 0, 5},
+		pred};
+
+}
