fc1c500611cc4b8259e03d0b6ed4276d8f95691c
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-02-14 12:40:48 +0300

    Update tuple_idioms_01.cpp

--- STL/tuple/tuple_idioms_01.cpp
+++ STL/tuple/tuple_idioms_01.cpp
@@ -1,13 +1,21 @@
-class Myclass {
+#include <tuple>
+#include <string>
 
+class Date {
+public:
+	//...
+	friend bool operator<(const Date&, const Date&);
+};
+
+class Myclass {
 public:
 	//...
 	friend bool operator<(const Myclass& r1, const Myclass& r2)
 	{
-		return std::tuple{ r1.mx, r1.mname, r1.mdate } < std::tuple{ r2.mx, r2.mname, r2.mdate };
+		return std::tuple{ r1.mx, r1.m_name, r1.m_date } < std::tuple{ r2.mx, r2.m_name, r2.m_date };
 	}
 private:
 	int mx{};
-	std::string mname{};
-	Date mdate{};
+	std::string m_name{};
+	Date m_date{};
 };
